f51c04570348ec3e37f6047fbc05cc5540f9fd12
leave1200 <markbarsaga121@gmail.com>
2024-07-04 22:13:28 +0800

    add

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = TRUE;
-
-
-// Add the webhook endpoint
-$route['webhook'] = 'webhook_controller/index';
