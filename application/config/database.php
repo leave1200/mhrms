db05485360c1f9d06b984f66b48ff51070089615
leave1200 <markbarsaga121@gmail.com>
2024-07-12 16:06:43 +0800

    a

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'u510162695',
+	'password' => '1Hrsystemci',
 	'database' => 'hrsystemci',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
