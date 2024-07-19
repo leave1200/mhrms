e4fe6e9d6d87663660dcab195eb6fc60147aa885
leave1200 <markbarsaga121@gmail.com>
2024-07-19 20:38:22 +0800

    a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('home', 'Home::index');
 
 
 $routes->group('admin', static function($routes){
