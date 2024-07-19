263a1d064570662b9599cde5037f3cd455fb7547
leave1200 <markbarsaga121@gmail.com>
2024-07-19 12:19:48 +0800

    a

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => '127.0.0.1',
+        'username'     => 'u510162695_hrsystemci',
+        'password'     => 'u510162695_hrsystemci',
+        'database'     => 'u510162695_hrsystemci',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
