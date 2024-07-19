e0eaf794d8664ffc66cbd0b61feb5dd71a4aa72c
leave1200 <markbarsaga121@gmail.com>
2024-07-19 11:33:34 +0800

    a

--- app/Views/backend/layout/inc/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer-wrap pd-20 mb-20 card-box">
-					Develop by Mark Rebb Barsaga - DeskApp template
-					<a href="https://github.com/dropways" target="_blank"
-						></a
-					>
-				</div>
