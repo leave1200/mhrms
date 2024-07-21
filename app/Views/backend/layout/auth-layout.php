7631d324345dbbaf38c86b026b6f923027a372b0
leave1200 <markbarsaga121@gmail.com>
2024-07-22 07:49:41 +0800

    a

--- app/Views/backend/layout/auth-layout.php
+++ app/Views/backend/layout/auth-layout.php
@@ -100,7 +100,7 @@
 			<div class="container">
 				<div class="row align-items-center">
 					<div class="col-md-6 col-lg-7">
-						<img src="/backend/vendors/images/login-page-img.png" alt="" />
+						<img src="" alt="" />
 					</div>
 					<div class="col-md-6 col-lg-5">
 						<?= $this->renderSection('content') ?>
