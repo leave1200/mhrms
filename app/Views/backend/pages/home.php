f9ecc76577108fdf5c0fccbd436ac3dd36cf33a4
leave1200 <markbarsaga121@gmail.com>
2024-07-22 08:20:00 +0800

    a

--- app/Views/backend/pages/home.php
+++ app/Views/backend/pages/home.php
@@ -18,49 +18,51 @@
 					</div>
 				</div><div class="row clearfix progress-box">
 					<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-						<div class="card-box pd-30 height-100-p">
+						<div class="card-box pd-30 height-100-p" data-bgcolor="#F19EF7">
 							<div class="progress-box text-center">
-								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial1" value="<?= $designationCount ?>" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#fff" data-fgcolor="#1b00ff" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: rgb(27, 0, 255); padding: 0px; appearance: none;font-size: 50px"></div>
-								<h5 class="text-blue padding-top-10 h5">Deparment</h5>
-								<span class="d-block"><i class="fa fa-line-chart text-blue"></i></span>
+								<div style="display:inline;width:120px;height:120px;">
+									<canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas>
+									<input type="text" class="knob dial1" value="<?= $designationCount ?>" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#F19EF7" data-fgcolor="#1b00ff" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: #fff; padding: 0px; appearance: none;font-size: 50px"></div>
+								<h5 class="text-white padding-top-10 h5">Deparment</h5>
+								<span class="d-block"><i class="fa fa-line-chart text-white"></i></span>
 							</div>
 						</div>
 					</div>
 					
-					<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-						<div class="card-box pd-30 height-100-p">
+					<div class="col-lg-3 col-md-6 col-sm-12 mb-30" >
+						<div class="card-box pd-30 height-100-p" data-bgcolor="#0079FA">
 							<div class="progress-box text-center">
 								<div style="display:inline;width:120px;height:120px;">
 									<canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas>
-									<input type="text" class="knob dial2" value="<?= $employeeCount ?>" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#fff" data-fgcolor="#00e091" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: rgb(0, 224, 145); padding: 0px; appearance: none;font-size: 50px">
+									<input type="text" class="knob dial2" value="<?= $employeeCount ?>" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#0079FA" data-fgcolor="#00e091" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: white; padding: 0px; appearance: none;font-size: 50px">
 								</div>
-								<h5 class="text-light-green padding-top-10 h5">
+								<h5 class="text-white padding-top-10 h5">
 									Employee
 								</h5>
-								<span class="d-block"><i class="fa text-light-green fa-line-chart"></i></span>
+								<span class="d-block"><i class="fa text-white fa-line-chart"></i></span>
 							</div>
 						</div>
 					</div>
 
 					<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-						<div class="card-box pd-30 height-100-p">
+						<div class="card-box pd-30 height-100-p" data-bgcolor="#F1975D">
 							<div class="progress-box text-center">
-								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial3" value="unvalue yet" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#fff" data-fgcolor="#f56767" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: rgb(245, 103, 103); padding: 0px; appearance: none;"></div>
-								<h5 class="text-light-orange padding-top-10 h5">
+								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial3" value="unvalue yet" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#F1975D" data-fgcolor="#f56767" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: white; padding: 0px; appearance: none;"></div>
+								<h5 class="text-white padding-top-10 h5">
 									Approved Leave Application
 								</h5>
-								<span class="d-block"> <i class="fa text-light-orange fa-line-chart"></i></span>
+								<span class="d-block"> <i class="fa text-white fa-line-chart"></i></span>
 							</div>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-						<div class="card-box pd-30 height-100-p">
+						<div class="card-box pd-30 height-100-p" data-bgcolor="#F15A9A" >
 							<div class="progress-box text-center">
-								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial4" value="Unvalue yet" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#fff" data-fgcolor="#a683eb" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: rgb(166, 131, 235); padding: 0px; appearance: none;"></div>
-								<h5 class="text-light-purple padding-top-10 h5">
+								<div style="display:inline;width:120px;height:120px;"><canvas width="150" height="150" style="width: 120px; height: 120px;"></canvas><input type="text" class="knob dial4" value="Unvalue yet" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgcolor="#F15A9A" data-fgcolor="#a683eb" data-angleoffset="180" readonly="readonly" style="width: 64px; height: 40px; position: absolute; vertical-align: middle; margin-top: 40px; margin-left: -92px; border: 0px; background: none rgb(255, 255, 255); font: bold 24px Arial; text-align: center; color: white; padding: 0px; appearance: none;"></div>
+								<h5 class="text-white padding-top-10 h5">
 									Panding Leave Application
 								</h5>
-								<span class="d-block"> <i class="fa text-light-purple fa-line-chart"></i></span>
+								<span class="d-block"> <i class="fa text-white fa-line-chart"></i></span>
 							</div>
 						</div>
 					</div>
