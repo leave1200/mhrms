5069996efcf3a8b573dc197deaf65f36ad0b2933
leave1200 <markbarsaga121@gmail.com>
2024-08-01 11:38:59 +0800

    a

--- app/Views/backend/pages/employee.php
+++ app/Views/backend/pages/employee.php
@@ -12,7 +12,7 @@
             </div>
             <nav aria-label="breadcrumb" role="navigation">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="<?= route_to('admin.home') ?>">Home</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Add Employee</li>
                 </ol>
             </nav>
